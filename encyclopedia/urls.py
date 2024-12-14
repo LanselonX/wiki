e5c735fe6df0ee8eae1d6c5e97fcf32734541b36
LanselonX <Lanselon1221@gmail.com>
2024-12-15 00:05:19 +0200

    added new with new functionality

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry_func, name="entry"),
-    path("search/", views.search_func, name="search"),
+    path("search", views.search_func, name="search"),
+    path("add", views.add, name="add"),
+    path("edit/<str:title>", views.edit, name="edit"),
+    path("random", views.random_title, name="random"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,32 @@
-import markdown2
+import markdown2, random
 
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django import forms
+from django.urls import reverse
+from django.contrib import messages
 
 from . import util
 
+class NewAddForm(forms.Form):
+    article = forms.CharField(label="Article")
+    content = forms.CharField(
+        label="Article Content",
+        widget=forms.Textarea(attrs={
+            "style": "width: 600px; height: 120px",
+            "placeholder": "Enter a description",
+        }),
+        required=False
+    )
+
+class EditForm(forms.Form):
+    content = forms.CharField(
+        label='',
+        widget=forms.Textarea(attrs={
+            "style": "width: 600px; height: 120px",
+            "placeholder": "Enter a description",
+        }),
+        required=False
+    )
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -33,7 +55,7 @@ def search_func(request):
     find_entries = list()
 
     if search_container in storage:
-        return redirect(f"/wiki/{search_container}")
+        return redirect(reverse("entry", kwargs={'title': search_container}))
 
     for entry in storage:
         if search_container.lower() in entry.lower():   
@@ -48,3 +70,52 @@ def search_func(request):
         return render(request, "encyclopedia/error.html", {
             "title": search_container
         })
+
+def add(request):
+    if request.method == "POST":
+        form = NewAddForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["article"]
+            content = form.cleaned_data["content"]
+            if util.get_entry(title):
+                form.add_error('article', "Error: article with this title already exist")
+            else:
+                util.save_entry(title, content)
+                messages.success(request, f"New page {title} created successfully")
+                return redirect(reverse('entry', args=[title]))
+        else:
+            NewAddForm()
+        return render(request, "encyclopedia/add.html", {
+            "form": form,
+        })
+                
+    return render(request, "encyclopedia/add.html", {
+        "form": NewAddForm(),
+    })
+
+def edit(request, title):
+    if request.method == "POST":
+        form = EditForm(request.POST)
+        if form.is_valid():
+            content = form.cleaned_data["content"]
+            util.save_entry(title, content)
+            messages.success(request, f"Entry {title} updated succussfuly")
+            return redirect(reverse('entry', args=[title]))
+        else:
+            messages.error(request, f"Editing form not valid, please try again!")
+    else:
+        existing_content = util.get_entry(title)
+        if existing_content is None:
+            messages.error(request, f"{title} not found")
+            return redirect(reverse('index'))
+        form = EditForm(initial={"content": existing_content})
+    return render(request, "encyclopedia/edit.html", {
+        "form": form,
+        "title": title,
+})
+
+def random_title(request):
+    titles = util.list_entries()
+    title = random.choice(titles)
+
+    return redirect(reverse('entry', args=[title]))
